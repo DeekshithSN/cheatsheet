2b0a09dd2293fbe91f61d41e57987286a057b000
Deekshith SN <deekshithsn@gmail.com>
2021-10-07 07:58:35 +0530

    Update docker-install.sh

--- docker-install.sh
+++ docker-install.sh
@@ -1,7 +1,7 @@
 sudo apt-get update -y
-sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-key fingerprint 0EBFCD88
 sudo apt-get update -y
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y 
